b7aebff9bb8d52adee2122b2745bc404f3891e1a
Jose Leon <leonj1@gmail.com>
2017-05-23 07:02:30 -0400

    Unit test proving that our logic should work

--- services/fetchprices_test.go
+++ services/fetchprices_test.go
@@ -6,23 +6,19 @@ import (
 )
 
 func TestSum(t *testing.T) {
-
-	i := 1
-
-	arr := []string{"item", "item", "item", "item", "item", "item"}
-
-	for _, name := range arr {
+	i := 0
+	arr := []string{"item", "item", "item", "item", "item"}
+	for idx, name := range arr {
 		switch name {
 		case "item":
-			if i == 3 {
+			if idx == 3 {
 				continue
 			}
 			i++
+			fmt.Println(fmt.Sprintf("idx: %d i: %d", idx, i))
 		}
 	}
-
-	fmt.Println(i)
 	if i != 4 {
-		t.Error("We expet i to be 4")
+		t.Errorf("We expet i to be 4, actual %d", i)
 	}
 }
