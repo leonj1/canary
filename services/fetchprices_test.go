bca505e9314201b68de9edadfc4871ca3ce9b431
Jose Leon <leonj1@gmail.com>
2017-05-23 06:39:52 -0400

    Stubbing out unit test

--- services/fetchprices_test.go
+++ services/fetchprices_test.go
@@ -1,1 +1,25 @@
 package services
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSum(t *testing.T) {
+
+	i := 1
+
+	arr := []string{"moe", "jose", "jose", "jose", "jose", "jose", "jose"}
+
+	for _, name := range arr {
+		//fmt.Println(name)
+		switch name {
+		case "jose":
+			if i == 3 {
+				continue
+			}
+			fmt.Println(i)
+			i++
+		}
+	}
+}
