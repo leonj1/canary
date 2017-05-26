d800e7033435a21a6ff503dcb512ab65d2a24813
Jose Leon <leonj1@gmail.com>
2017-05-26 12:01:32 -0400

    Adding unit test to cover exception

--- services/fetchprices_test.go
+++ services/fetchprices_test.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"errors"
 	"fmt"
 	"testing"
 )
@@ -13,6 +14,32 @@ func TestSum(t *testing.T) {
 		case "item":
 			if idx == 3 {
 				continue
+			}
+			i++
+			fmt.Println(fmt.Sprintf("idx: %d i: %d", idx, i))
+		}
+	}
+	if i != 4 {
+		t.Errorf("We expet i to be 4, actual %d", i)
+	}
+}
+
+func forceError() error {
+	return errors.New("Forcing this error")
+}
+
+func TestHandleException(t *testing.T) {
+	i := 0
+	arr := []string{"item", "item", "item", "item", "item"}
+	for idx, name := range arr {
+		switch name {
+		case "item":
+			if idx == 3 {
+				err := forceError()
+				if err != nil {
+					fmt.Println("Exception gets thrown here")
+					continue
+				}
 			}
 			i++
 			fmt.Println(fmt.Sprintf("idx: %d i: %d", idx, i))
